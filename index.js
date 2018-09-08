8ee4585d62a666cfe4bf350428340d3421caaade
Matt Watt <matt@karwalski.net>
2018-09-08 17:59:47 +1000

    added additional intents

--- index.js
+++ index.js
@@ -46,21 +46,47 @@ function dispatch(intentRequest, callback) {
     const userId = intentRequest.userId;
     const intentName = intentRequest.currentIntent.name;
     
+    const age = parseInt(slots.age);
+    const gender = slots.gender;
+    
     const rand = Math.floor(Math.random() * 65536);
     
    
+    const weight = parseInt(slots.weight);
+    const height = parseInt(slots.height);
+    var bmi = parseInt(slots.bodymassindex);
     
     
         // DB table settings
     var AWS = require("aws-sdk");
     var docClient = new AWS.DynamoDB.DocumentClient();
-    var table = "govHack";
+    var table = "govHack_user";
 
-if (intentName === 'intro')
+
+
+if (intentName === 'greeting')
 {
     
     // Introduction intent - testing
 
+    table = "govHack_user";
+
+    // Write to table
+    var params = {
+    TableName:table,
+    Item:{
+        "id" : userId,
+        "name": name
+    }
+};
+
+docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
 
     callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Hello ${name}, I am a chatbot`}));
 
@@ -68,20 +94,128 @@ if (intentName === 'intro')
 }
 
 
-
-
-
-    // Write to table
-    var params = {
-    TableName:table,
-    Item:{
-        "id" : rand,
-        "name": name,
+if (intentName === 'bmi')
+{
+    if(!bmi)
+    {
+        bmi = Math.round(weight / (height/100) / (height/100));
     }
+    
+    // if entered BMI should have a codehook and skip asking weight and height
+
+    // callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Your BMI is ${bmi}`}));
+    
+    
+    //Read from table
+    table = "bmi";
+    
+    var bmiCatagory;
+    var bmiDescription = ["Underweight", "Normal weight", "Overweight", "Obese"] ;
+    if (bmi < 18)
+    {
+        bmiCatagory = 0;
+    }
+    else if (bmi < 25)
+    {
+        bmiCatagory = 1;
+    }
+    else if (bmi < 30)
+    {
+        bmiCatagory = 2;
+    }
+    else
+    {
+        bmiCatagory = 3;
+    }
+    
+params = {
+    TableName: table,
+            Key: {
+    "id" : bmiCatagory
+  }
 };
 
 
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Error.`}));
+    } else {
+        console.log("Query succeeded.");
+var response = "";
+       
+       var responseLink = "http://www.health.gov.au/internet/main/publishing.nsf/Content/phy-activity";
+       
+        if(gender == "male" || gender == "Male")
+        {
+            // response = JSON.stringify(data);
+           response = data.Item.male;
+           // http://www.health.gov.au/internet/main/publishing.nsf/Content/phy-activity
+        }
+        else
+        {
+           response = data.Item.female;
+           // https://campaigns.health.gov.au/girlsmove
+           
+           responseLink = "https://campaigns.health.gov.au/girlsmove";
+        }
+        // response = JSON.stringify(data);
+   
+        
+        callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Your BMI is ${bmi} (${bmiDescription[bmiCatagory]}). ${response}% of ${gender}\'s are ${bmiDescription[bmiCatagory]}. Visit ${responseLink} for the benefits.`}));
+    }
+});
+
+}
+
+
+
+
+
+
+if (intentName === 'physical_activity')
+{
+    //Read from table
+    table = "physical_activity";
     
+params = {
+    TableName: table,
+            Key: {
+    "age" : age
+  }
+};
+
+
+docClient.get(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `Error.`}));
+    } else {
+        console.log("Query succeeded.");
+var response = "";
+       
+       var responseLink = "http://www.health.gov.au/internet/main/publishing.nsf/Content/phy-activity";
+        if(gender == "male" || gender == "Male")
+        {
+            // response = JSON.stringify(data);
+           response = data.Item.male;
+           
+        }
+        else
+        {
+           response = data.Item.female;
+           responseLink = "https://campaigns.health.gov.au/girlsmove";
+        }
+        // response = JSON.stringify(data);
+   
+        
+        callback(close(sessionAttributes, 'Fulfilled', {'contentType': 'PlainText', 'content': `${response}% of ${age} year old ${gender}\'s are physically active atleast 5 times for 30 minutes in a week. Visit ${responseLink} for the benefits.`}));
+    }
+});
+    
+    
+    
+}    
 
 
 
